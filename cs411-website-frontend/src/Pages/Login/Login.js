5af9b7a95c9a71cde826c9c4e53da00895f7050e
brendaaurelie <brenda.aurelie@gmail.com>
2022-03-28 22:43:55 -0500

    log in page

--- cs411-website-frontend/src/Pages/Login/Login.js
+++ cs411-website-frontend/src/Pages/Login/Login.js
@@ -1,8 +1,75 @@
-import React from 'react'
+import React from "react";
+import Button from "@mui/material/Button";
+import { useState } from "react";
+import { TextField } from "@mui/material";
+import { makeStyles } from "@material-ui/core";
+import "./Login.css";
+
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      padding: theme.spacing(10),
+      backgroundColor: "white",
+      "& .MuiTextField-root": {
+        margin: theme.spacing(2),
+        width: "300px",
+      },
+      "& .MuiButtonBase-root": {
+        margin: theme.spacing(2),
+        width: "300px",
+      },
+    },
+  }));
 
 const Login = () => {
+    
+    const classes = useStyles();
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     return(
-        <h1>Im in Login</h1>
+    <div className="LogIn">
+      <form className={classes.root}>
+        <h className="title">Welcome Back!</h>
+    
+        <TextField
+          label="Email"
+          variant="outlined"
+          type="email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          label="Password"
+          variant="outlined"
+          type="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <div>
+          <Button className="button1" type="submit" variant="contained" style={{backgroundColor:'#12565a'}} >
+            Log In
+          </Button>
+
+          <p className="SignRout">
+            {" "}
+            Don't have an account?
+            <Link className="SignRout_b" to="/SignUp">
+              {" "}
+              Log-In
+            </Link>
+          </p>
+        </div>
+      </form>
+    </div>
     );
 
 };
